875ab707c99ba33a279a78c2fa8933f2a284831c
David Hammett <david@cloudedskies.com>
2026-06-15 22:00:28 +0100

    Continue implementation of Azure login

--- .github/scripts/InstallCertificates.ps1
+++ .github/scripts/InstallCertificates.ps1
@@ -1,15 +1,17 @@
 param(
+	[switch]$ImportCert,
 	[Parameter(Mandatory=$true)]
     [string]$RootCert,
     [string]$IntermediateCert,
 	[Parameter(Mandatory=$true)]
 	[string]$TenantId,
 	[Parameter(Mandatory=$true)]
-	[string]$KeyVaultUrl,
+	[string]$KeyVaultName,
 	[Parameter(Mandatory=$true)]
 	[string]$CertName
 )
 
+$codeSigningCertPath = "$($env:TEMP)\CodeSigning.pfx"
 $rootCertPath = "$($env:TEMP)\Root.cer"
 $intermediateCertPath = "$($env:TEMP)\Intermediate.cer"
 
@@ -45,4 +47,18 @@ try {
 } catch {
 	Write-Host "Importing intermediate cert into cert store failed. $($_.Exception.Message)"
 	exit 1
+}
+
+if ($ImportCert.IsPresent) {
+	try {
+		$pemCert = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $CertName -AsPlainText -ErrorAction Stop
+		$pfxCert = [Convert]::FromBase64String($pemCert)
+		$pfxCert | Out-File $codeSigningCertPath -ErrorAction Stop
+		$cert = Import-PfxCertificate -FilePath $codeSigningCertPath -CertStoreLocation "Cert:\LocalMachine\My" -ErrorAction Stop
+		"Thumbprint=$($cert.Thumbprint)" | Out-File -FilePath $env:GITHUB_OUTPUT -Append -ErrorAction Stop
+		Remove-Item -Path $codeSigningCertPath -Force -Confirm:$false -ErrorAction SilentlyContinue
+	} catch {
+		Write-Host "Importing certificate failed. $($_.Exception.Message)"
+		exit 1
+	}
 }
